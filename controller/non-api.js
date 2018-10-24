9de570f2ae7439b31660522fd8f383ca798cd4ad
Samrat Dey <mail.samrat.dey@gmail.com>
2018-10-25 03:58:44 +0530

    email confirmation checkups

--- controller/non-api.js
+++ controller/non-api.js
@@ -1,4 +1,5 @@
 const UserModel = require('../model/user');
+const moment = require('moment');
 
 module.exports = {
 
@@ -9,20 +10,51 @@ module.exports = {
 		if( getToken ) {
 			UserModel.findByEmailToken(getToken)
 				.then(user => {
-					user.emailVerification[0].isverified = true;
-					user.save()
-						.then(updatedUser => {
-							res.send({ status: true });
-						})
-						.catch(err => {
-							res.status(422).send({ message: 'invalid token' });
-						});
+					let start = moment(user.createdAt);
+					let now = moment(new Date());
+					let duration = moment.duration(now.diff(start));
+					let days = duration.asDays();
+
+					if( user.emailVerification[0].isverified === true ) {
+						res.status(200).send({ message: 'email is already verified' });
+						return;
+					}
+
+					if( days > 3 ) {
+						user.emailVerification = [];
+					} else {
+						user.emailVerification[0].isverified = true;
+					}
+
+					if( days > 3 ) {
+						user.save()
+							.then(updatedUser => {
+								if( updatedUser ) {
+									res.status(403).send({ message: 'token expired' });
+								} else {
+									res.status(422).send({ message: 'email verification reset has failed.' });
+								}
+							})
+							.catch(err => {
+								res.status(422).send(err);
+							});
+						
+					} else {
+						user.save()
+							.then(updatedUser => {
+								res.status(200).send({ message: 'successfully verified' });
+							})
+							.catch(err => {
+								res.status(401).send({ message: 'invalid token' });
+							});
+						} // endif
+
 				})
 				.catch(err => {
-					res.status(422).send({ message: 'invalid token' });
+					res.status(401).send(err);
 				});
 		} else {
-			res.send({ status: false });
+			res.status(401).send({ message: 'invalid token' });
 		}
 
 
